48fa318a3e53d3e9ba0d43d48d2af4dd6be9d5ab
eshwar93 <eshwar9319@gmail.com>
2025-05-31 17:51:19 +0530

    modified the main.tf file

--- tf/main.tf
+++ tf/main.tf
@@ -10,10 +10,6 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
     features {}
-  subscription_id   = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
-  client_id         = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
-  client_secret     = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
-  tenant_id         = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 }
 
 # Create a resource group
